a15f15c6c02c336c30a8f7ade2f66e915fd75441
Adam Gavlák <gavlak1@fri.uniza.sk>
2016-03-02 17:41:26 +0100

    Cvicenie 2

--- Structures/Vector.cpp
+++ Structures/Vector.cpp
@@ -1,6 +1,7 @@
 #include "Vector.h"
 #include "stdlib.h"
 #include "string.h"
+#include "DSRoutines.h"
 
 using namespace DS;
 
@@ -41,9 +42,11 @@ Structure* Vector::clone() const
 	return clone;
 }
 
-Structure & Vector::operator=(const Structure & other)
+Structure& Vector::operator=(const Structure & other)
 {
-	throw -1;
+	const Vector& temp = dynamic_cast<const Vector&>(other);
+	*this = temp;
+	return *this;
 }
 
 Vector& Vector::operator=(const Vector& other)
@@ -53,6 +56,8 @@ Vector& Vector::operator=(const Vector& other)
 		memory_ = realloc(memory_, size_);
 		memcpy(memory_, other.memory_, size_);
 	}
+
+	return *this;
 }
 
 bool Vector::operator==(const Vector& other) const
@@ -62,50 +67,97 @@ bool Vector::operator==(const Vector& other) const
 
 size_t Vector::size() const
 {
-	throw -1;
+	return size_;
 }
 
 byte& Vector::operator[](const int index)
 {
-	throw -1;
+	DSRoutines::rangeCheck(index, size_, true);
+
+	byte& temp = *(reinterpret_cast<byte*>(memory_) + index);
+
+	return temp;
 }
 
 byte Vector::operator[](const int index) const
 {
-	throw -1;
+	DSRoutines::rangeCheck(index, size_, true);
+
+	byte temp = *(reinterpret_cast<byte*>(memory_) + index);
+
+	return temp;
 }
 
 bool Vector::byteNthBitGet(int B, int n)
 {
-	throw -1;
+	DSRoutines::rangeCheck(B, size_, true);
+	DSRoutines::rangeCheck(n, 8, true);
+
+	return ((*this)[B] >> n) & 1;
 }
 
 byte Vector::byteNthBitTo0(int B, int n)
 {
-	throw -1;
+	DSRoutines::rangeCheck(B, size_, true);
+	DSRoutines::rangeCheck(n, 8, true);
+
+	return (*this)[B] &= ~(1 << n);
 }
 
 byte Vector::byteNthBitTo1(int B, int n)
 {
-	throw -1;
+	DSRoutines::rangeCheck(B, size_, true);
+	DSRoutines::rangeCheck(n, 8, true);
+
+	return (*this)[B] |= (1 << n);
 }
 
 byte Vector::byteNthBitToggle(int B, int n)
 {
-	throw -1;
+	DSRoutines::rangeCheck(B, size_, true);
+	DSRoutines::rangeCheck(n, 8, true);
+
+	return (*this)[B] ^= (1 << n);
 }
 
 byte& Vector::readBytes(const int index, const int count, byte& dest)
 {
-	throw -1;
+	DSRoutines::rangeCheck(index, size_, true);
+	DSRoutines::rangeCheck(index + count, size_ + 1, true);
+	DSRoutines::rangeCheck(count, size_ + 1, true);
+
+	memcpy(&dest, reinterpret_cast<byte*>(memory_) + index, count);
+	return dest;
 }
 
 void DS::Vector::copy(const Vector& src, const int srcStartIndex, Vector& dest, const int destStartIndex, const int length)
 {
-	throw -1;
+	DSRoutines::rangeCheck(srcStartIndex, src.size_, true);
+	DSRoutines::rangeCheck(srcStartIndex + length, src.size_, true);
+
+	DSRoutines::rangeCheck(destStartIndex, dest.size_, true);
+	DSRoutines::rangeCheck(destStartIndex + length, dest.size_, true);
+
+	DSRoutines::rangeCheck(length, src.size_ + 1, true);
+
+	if (&src != &dest) 
+	{
+		memcpy(reinterpret_cast<byte*>(dest.memory_) + destStartIndex,
+			   reinterpret_cast<byte*>(src.memory_) + srcStartIndex,
+			   length);
+	}
+	else 
+	{
+		memmove(reinterpret_cast<byte*>(dest.memory_) + destStartIndex,
+			    reinterpret_cast<byte*>(src.memory_) + srcStartIndex,
+		 	    length);
+	}
 }
 
 byte* DS::Vector::getByteAddress(const int index)
 {
-	throw -1;
+	DSRoutines::rangeCheck(index, size_, true);
+
+	// return &(*this)[index];
+	return reinterpret_cast<byte*>(memory_) + index;
 }
